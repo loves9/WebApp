dffdc4b29821cf2487ef2afcc8c7c92a184de910
loves9 <ifengfree@gmail.com>
2018-09-04 14:49:09 +0800

    [opt]baseurl

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  baseURL: '"http://192.168.1.130:3030"'
+  baseURL: '"http://localhost:3030"'
 })
